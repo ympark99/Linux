7ddea3835a27b4683339f1b5f0dd3f3921051905
ympark99 <ympark1230@naver.com>
2022-03-04 22:28:33 +0900

    시작 공백 제거 구현

--- ssu_sindex.c
+++ ssu_sindex.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h> // string 관련 함수 사용
+#include <stdlib.h> // realpath 사용
 #include "ssu_sindex.h"
 
 void ssu_sindex(){
@@ -13,9 +14,14 @@ void ssu_sindex(){
 		while(oper[0] == ' '){
 			memmove(oper, oper + 1, strlen(oper));
 		}
-
+		
+		// find 명령 시 find함수 실행
+		if(isFind(oper)){
+			memmove(oper, oper + 4, strlen(oper)); // find 문자열 제거
+			find(oper);
+		}
 		// exit 입력 시 종료
-		if(strcmp(oper, "exit\n") == 0){
+		else if(strcmp(oper, "exit\n") == 0){
 			printf("Prompt End\n");
 			break;
 		}
@@ -37,4 +43,30 @@ void print_inst(){
 	printf("   s : report when two files are the same\n");
 	printf("   i : ignore case differences in file contents\n");
 	printf("   r : recursivly compare any subdirectories found\n");
+}
+
+// find 명령어 확인 함수
+bool isFind(char oper[]){
+	for(int i = 0; i < 4; i++){
+		if(oper[0] == 'f' && oper[1] == 'i' && oper[2] == 'n' && oper[3] == 'd') return true;
+	}
+	return false;
+}
+
+void find(char oper[]){
+	char *findOper[1024] = {NULL, }; // FILENAME PATH등 find 이후 명령어 저장
+	
+	char *ptr = strtok(oper, " "); // 공백 기준으로 문자열 자르기
+
+	int idx = 0;
+	while (ptr != NULL){
+		findOper[idx] = ptr;
+		idx++;
+		ptr = strtok(NULL, " ");
+	}
+
+	for(int i = 0; i < sizeof(findOper) / sizeof(findOper[0]); i++){
+		if(findOper[i] != NULL)
+			printf("%s\n", findOper[i]);
+	}
 }
--- ssu_sindex.h
+++ ssu_sindex.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdbool.h> // bool, true, false 사용
 
 void ssu_sindex();
 void print_inst();
+
+bool isFind(char oper[]);
+void find(char oper[]);
 
 #endif
