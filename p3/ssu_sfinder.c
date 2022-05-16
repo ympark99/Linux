be8fb1edc6a7fc74465f1e602ed1e38d7f948de3
ympark99 <ympark1230@naver.com>
2022-05-16 13:13:17 +0000

    help 파일 제거

--- p3/ssu_sfinder.c
+++ p3/ssu_sfinder.c
@@ -401,3 +401,13 @@ int main(){
 	fprintf(stdout, "Prompt End\n");
 	exit(0);
 }
+
+void ssu_help(){
+	printf("Usage:\n");
+	printf("  > fmd5/fsha1  [FILE_EXTENSION] -l [MINSIZE] -h [MAXSIZE] -d [TARGET_DIRECTORY] -t [THREAD_NUM]\n");
+	printf("     >>  delete -l [SET_INDEX] -d [OPTARG] -i -f -t\n");
+	printf("  > trash -c [CATEGORY] -o [ORDER]\n");
+	printf("  > restore [RESTORE_INDEX]\n");
+	printf("  > help\n");
+	printf("  > exit\n\n");
+}
